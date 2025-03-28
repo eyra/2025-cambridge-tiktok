3bb8f5a75084290b643a71a5611b149896ba0fdf
Adrienne Mendrik <79082794+AdrienneMendrik@users.noreply.github.com>
2025-03-28 10:16:26 +0100

    Revert "Merge branch 'master' of https://github.com/eyra/2025-cambridge-tiktok"
    This reverts commit d55d91e3bde5d0fb4d02cfd621d583d2701b9ffa, reversing
    changes made to dff2a92b6deb19ba1cf00ef5d4aa97da2a937dc2.

--- packages/feldspar/rollup.config.js
+++ packages/feldspar/rollup.config.js
@@ -1,4 +1,5 @@
-import fs from "fs";
+import fs from 'fs';
+import path from 'path';
 
 // Read package.json directly
 const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
@@ -6,16 +7,14 @@ const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 // Export a function that returns the config to allow for async plugin loading
 export default async function() {
   // Dynamically import plugins
-  const typescript = (await import("@rollup/plugin-typescript")).default;
-  const { nodeResolve } = await import("@rollup/plugin-node-resolve");
-  const commonjs = (await import("@rollup/plugin-commonjs")).default;
-  const terser = (await import("@rollup/plugin-terser")).default;
-  const peerDepsExternal = (await import("rollup-plugin-peer-deps-external"))
-    .default;
-  const dts = (await import("rollup-plugin-dts")).default;
-  const url = (await import("@rollup/plugin-url")).default;
-  const json = (await import("@rollup/plugin-json")).default;
-  const copy = (await import("rollup-plugin-copy")).default;
+  const typescript = (await import('@rollup/plugin-typescript')).default;
+  const { nodeResolve } = await import('@rollup/plugin-node-resolve');
+  const commonjs = (await import('@rollup/plugin-commonjs')).default;
+  const { terser } = await import('rollup-plugin-terser');
+  const peerDepsExternal = (await import('rollup-plugin-peer-deps-external')).default;
+  const dts = (await import('rollup-plugin-dts')).default;
+  const url = (await import('@rollup/plugin-url')).default;
+  const json = (await import('@rollup/plugin-json')).default;
 
   return [
     {
@@ -53,14 +52,6 @@ export default async function() {
           resolveJsonModule: true
         }),
         terser(),
-        copy({
-          targets: [
-            { 
-              src: 'src/fonts/*', 
-              dest: 'dist/fonts' 
-            }
-          ]
-        })
       ],
       external: ['react', 'react-dom', ...Object.keys(packageJson.dependencies || {})],
     },
