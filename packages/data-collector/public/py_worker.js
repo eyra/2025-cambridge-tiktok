c8f02dfadf37b51d51a4027bbd5c92e9b1d64376
Jeroen <jeroen@adaptiveconstant.com>
2025-11-07 13:46:27 +0100

    Upgrade Pyodide to latest

--- packages/data-collector/public/py_worker.js
+++ packages/data-collector/public/py_worker.js
@@ -95,11 +95,11 @@ function initialise() {
 }
 
 function startPyodide() {
-  importScripts("https://cdn.jsdelivr.net/pyodide/v0.24.0/full/pyodide.js");
+  importScripts("https://cdn.jsdelivr.net/pyodide/v0.29.0/full/pyodide.js");
 
   console.log("[ProcessingWorker] loading Pyodide");
   return loadPyodide({
-    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.24.0/full/",
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.29.0/full/",
   });
 }
 
